164c491141b987993c4264b49cbd405676bae994
asukaokochi <x17018xx@gmail.com>
2018-06-07 21:18:45 +0900

    ゲーム開始時のメッセージ

--- src/user.js
+++ src/user.js
@@ -1,2 +1,21 @@
-function userChat(){
+function userChat(userId,gotText,replyToken){
+    //（できれば）ルールって言われた時
+    if(gotText == "@ルール") {
+        
+    }
+    
+    // ゲーム中の個人メッセージ
+    //var gflug = SHEET_G.getRange(glow,4).getValue();
+    if(gflug == 4) {
+        message = {
+            "to": userId,
+            "messages": [
+                {
+                    "type": "text",
+                    "text": "Hello, user!"
+                }
+            ]
+        }
+        reply(message);
+    }
 }
